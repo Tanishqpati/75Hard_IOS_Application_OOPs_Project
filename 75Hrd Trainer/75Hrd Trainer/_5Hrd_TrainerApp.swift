fda2f54eda8d119e843797520d9260b6ddfd8dc5
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-09-25 18:22:41 +0530

    IOS app initial setup done

---
+++ 75Hrd Trainer/75Hrd Trainer/_5Hrd_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5Hrd_TrainerApp.swift
+//  75Hrd Trainer
+//
+//  Created by Tanishq Patidar on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct _5Hrd_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
